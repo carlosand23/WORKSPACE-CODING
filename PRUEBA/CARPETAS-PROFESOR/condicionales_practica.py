afbf6a64f775c183ffa0e816b0b8ce95e1ff3966
carlosand23 <carlosand23@hotmail.com>
2025-03-05 20:51:11 +0100

    condicionales_practica.py

---
+++ PRUEBA/CARPETAS-PROFESOR/condicionales_practica.py
@@ -0,0 +1,12 @@
+# Solicita dos números al usuario y determina cuál de los dos es mayor o si son iguales.
+Numero_1 = int(input("Ingrese el primer numero "))
+Numero_2 = int(input("Ingrese el segundo numero "))
+
+if Numero_1 > Numero_2:
+    print("el primer numero es mayor")
+elif Numero_2 > Numero_1:
+    print("el segundo numero es el mayor")
+elif Numero_1 or Numero_2 = str:
+    print("solo puedes colocar numeros")
+else:
+    print("los numeros son iguales")
