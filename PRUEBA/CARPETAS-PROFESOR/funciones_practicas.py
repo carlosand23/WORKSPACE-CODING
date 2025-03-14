efe0259327d58e3c393d5d3c26f9ac46da3e96a6
carlosand23 <carlosand23@hotmail.com>
2025-03-14 17:18:38 +0100

    trabajo_final_curso.py

---
+++ PRUEBA/CARPETAS-PROFESOR/funciones_practicas.py
@@ -0,0 +1,6 @@
+# Escribe una función llamada es_par que tome un número como parámetro 
+# y devuelva True si el número es par y False si es impar.
+def es_par(numero):
+    return numero %2 == 0
+    
+print(es_par(6))
